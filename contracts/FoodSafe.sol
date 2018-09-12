05a5ecc555a287e2983bdfc061930d29164fcb4d
smshafi <33542787+smshafi@users.noreply.github.com>
2018-09-12 16:02:21 +0530

    Update FoodSafe.sol
    upgrade solidity 0.4.11

--- contracts/FoodSafe.sol
+++ contracts/FoodSafe.sol
@@ -1,1 +1,40 @@
-pragma solidity ^0.4.6;contract FoodSafe { struct Location {    string name;  uint locationID;   uint previousLocationID;   string secret;    uint timestamp;} mapping(uint=>Location) Trail;uint8 trailcount=0;function addNewLocation(string name,uint locationID,string secret){    Location memory newLocation;    newLocation.name=name;    newLocation.locationID=locationID;    if (trailcount!=0)    {        newLocation.previousLocationID=Trail[trailcount-1].locationID;    }    newLocation.secret=secret;    newLocation.timestamp=now;    Trail[trailcount]=newLocation;    trailcount++;} function GetTrailCount() returns(uint8)  {    return trailcount;} function GetLocation(uint8 trailNo) returns(string,uint,uint,uint,string){    return (Trail[trailNo].name,Trail[trailNo].locationID,Trail[trailNo].previousLocationID,Trail[trailNo].timestamp,Trail[trailNo].secret);}}
+pragma solidity ^0.4.11;
+contract FoodSafe 
+{ 
+    struct Location 
+    {    
+        string name;  
+        uint locationID;   
+        uint previousLocationID;   
+        string secret;    
+        uint timestamp;
+    } 
+    mapping (uint => Location) Trail;
+    uint trailcount=0;
+    function addNewLocation(string name,uint locationID,string secret)
+    {    
+        Location memory newLocation;    
+        newLocation.name=name;    
+        newLocation.locationID=locationID;    
+        if (trailcount>0)    
+        {        
+            newLocation.previousLocationID=Trail[trailcount-1].locationID;    
+        
+        }    
+        newLocation.secret=secret;    
+        newLocation.timestamp=now;    
+        Trail[trailcount]=newLocation;    
+        trailcount=trailcount+1;
+            
+    } 
+    function GetTrailCount() returns (uint) 
+    { 
+        return trailcount;
+    }
+    function GetLocation(uint trailNo) returns (string,uint,uint,uint,string)
+    {    
+        return (Trail[trailNo].name,Trail[trailNo].locationID,Trail[trailNo].previousLocationID,Trail[trailNo].timestamp,Trail[trailNo].secret);
+        
+    }
+    
+}
